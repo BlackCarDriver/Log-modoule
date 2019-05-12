5f7568121616bbfa4bb9ec40834131acce296ab9
61602008 <724608767@qq.com>
2019-05-12 22:13:38 +0800

    orelog2.0

--- logServer/logs/database.go
+++ logServer/logs/database.go
@@ -9,18 +9,29 @@ import (
 //pointer of database
 var db *sql.DB
 
+const (                      //数据库登入信息
+    host     = "localhost"
+    port     =  5432
+    user     = "Dong"
+    password = "87257745"
+    dbname   = "management"
+)
+
 func testconnect() {
 	var err error
 	//if don't add 'sllmod=disable' there may export error "pq: SSL is not enabled on the server"
-	confstr := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		"129.204.193.192",
-		5432,
-		"logmoduleuser",
-		"itispassword",
-		"logmodule",
-	)
-	db, err = sql.Open("postgres", confstr)
+	// confstr := fmt.Sprintf(
+	// 	"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	// 	"129.204.193.192",
+	// 	5432,
+	// 	"logmoduleuser",
+	// 	"itispassword",
+	// 	"logmodule",
+	// )
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+    db, err = sql.Open("postgres", psqlInfo)
+	//db, err = sql.Open("postgres", confstr)
 	err = db.Ping()
 	if err!= nil {
 		panic(err)
