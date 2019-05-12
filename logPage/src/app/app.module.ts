d1f2e9bd12eab6ee01359829c373c3795f872e7c
BlackCarDriver <13169346002@163.com>
2019-05-12 13:59:07 +0800

    working on log-page

--- logPage/src/app/app.module.ts
+++ logPage/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LogPageComponent } from './log-page/log-page.component';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -10,9 +18,13 @@ import { LogPageComponent } from './log-page/log-page.component';
     LogPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- logPage/src/app/log-page/log-page.component.ts
+++ logPage/src/app/log-page/log-page.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-page.component.css']
 })
 export class LogPageComponent implements OnInit {
-
   constructor() { }
-
-  ngOnInit() {
+    ngOnInit() {
   }
+
+  panels = [
+    { name: '2019-5-11',
+      list:['第一次构建','第二次构建','第三次构建',]
+  },
+    { name: '2019-5-12',
+    list:['第一次构建','第二次构建','第三次构建',]
+},
+{ name: '2019-5-11',
+list:['第一次构建','第二次构建','第三次构建',]
+},
+{ name: '2019-5-12',
+list:['第一次构建','第二次构建','第三次构建',]
+},
+{ name: '2019-5-11',
+list:['第一次构建','第二次构建','第三次构建',]
+},
+{ name: '2019-5-12',
+list:['第一次构建','第二次构建','第三次构建',]
+}
+
+  ];
 
 }
