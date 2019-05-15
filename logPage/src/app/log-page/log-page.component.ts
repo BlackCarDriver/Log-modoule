5dcc4c72fb32274a3b715c66244aa5d902f16bd7
BlackCarDriver <13169346002@163.com>
2019-05-16 00:19:42 +0800

    rebuild client

--- logPage/src/app/log-page/log-page.component.ts
+++ logPage/src/app/log-page/log-page.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ServerService } from '../server.service';
-import {  loglist , codeformat, Orelog } from '../struct';
-// import { ApiSerivice } from '../apiservice';
-// import { ActivatedRoute, Router } from '@angular/router';
-import * as $ from 'jquery'
-// import { Orelog } from '../apiresponse';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-log-page',
@@ -14,22 +9,15 @@ import * as $ from 'jquery'
 })
 
 export class LogPageComponent implements OnInit {
-  constructor(private server: ServerService) { }
-    ngOnInit() {
-      this.Getorelog();
-    this.Getloglist(0);
-     this.Getlogtext("2019-05-12_1","info.log");
-  }
+  private addr: string  = "http://localhost:8090";
+  // private addr:string= "api";
+
+  constructor(private http: HttpClient) { }
+  ngOnInit() {}
 
   pageIndex = 1;  //操作日志首页码
   pageSize = 10;   //操作日志每页条数
   oreloglist = []   //操作日志
-
-  filename = "";  //当前日志文本框显示的日志的文件名和路径
-  page = 0; //当前浏览的是日志列表的第几页
-  list = loglist[50]; //日志目录
-  logtext = [];  //日志文本
-
   
   // tslint:disable-next-line:member-ordering
   public orelogs: Orelog[];
@@ -41,38 +29,46 @@ export class LogPageComponent implements OnInit {
     module:'',
     logsql:''
   };
-
-  //请求获取系统日志的目录列表
-  Getloglist(page :number){
-    this.server.GetLogCatalog(page).subscribe(result=>{
-        this.list = result;
-        console.log(this.list);
-    });
-  }
-
-  //请求获取特定日志的文本内容
-  Getlogtext(tag:string, type:string){
-    this.server.GetLogText(tag,type).subscribe(result=>{
-      this.logtext = result;
-    });
-  }
-
-  updatelogtext(date:string,input:number, ty:string){
-    var tag = date +"_"+input;
-    this.Getlogtext(tag, ty);
-  }
-
+  listOfData = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
   // 请求操作日志内容
   public Getorelog() {           // 获取信息
-    this.server.getorelog().subscribe(response => {
+    let url = this.addr + '/log/logdisplay';
+    this.http.get<Orelog[]>(url).subscribe(response => {
       this.orelogs = response;
       //var testtrimresult = orelog.substring(0, testtrim.length-2);
     });
   }
 
   // 操作日志分页
-searchData(): void {
+  searchData(): void {
     this.oreloglist = this.orelogs.slice((this.pageIndex - 1) * this.pageSize, (this.pageIndex) * this.pageSize);
   }
 
+}
+
+class Orelog {
+  public logid:       number;
+  public logtime:     string;
+  public admin:       string;
+  public module:      string;
+  public logsql:      string;
 }
