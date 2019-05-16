e6d52b1023ed54db33a4b80dd6eb6eb73682289f
61602008 <724608767@qq.com>
2019-05-17 00:57:30 +0800

    后端

--- logServer/logs/logs.go
+++ logServer/logs/logs.go
@@ -1,202 +1,46 @@
 package logs
 
 import (
-	"fmt"
-	"log"
-	"runtime"
-	"strings"
-	"time"
-	"os"
+	//"../models"
+	"encoding/json"
 	"io/ioutil"
-	"strconv"
-	"database/sql"
+	"net/http"
 )
 
-const(
-	// is the path saving the logs files
-	logs_root= `./logs/logsfile/`
-	// you can set mew_log = 0 when you are testing, then it will cover the oldest flode to save the logsfiles 
-	//note that new_log should be 0 or 1 !
-	new_log = 0
-)
-
-//the following constant are the paramater used in Log() style
-const (
-	Err = 400
-	Warn = 200
-	Q_err =-400
-	Q_warn =-200
-)
-
-var(
-	error_log_name = "error.log"
-	warn_log_name = "warning.log"
-	info_log_name = "info.log"
-)
-
-var errlog *log.Logger
-var warnlog *log.Logger
-var infolog *log.Logger
-
-
-func init() {
-	log_floder := createfloder()
-	errorp, err := os.Create(log_floder + error_log_name)
-	warnp, _ := os.Create(log_floder + warn_log_name)
-	infop, _ := os.Create(log_floder + info_log_name)
-	if err != nil  {
-		panic("can not create logsfile!")
-	}
-	//if close the *File,the log can't be writen in file!
-	errlog = log.New(errorp, "", 3)
-	warnlog = log.New(warnp, "", 3)
-	infolog = log.New(infop, "", 3)
+func Records(admin string, content string){       //添加日志(登录)
+	log.Println("正在添加日志...")
+    rows, err := db.Prepare("insert into t_opelog (admin,content) values($1,$2)")
+    checkErr(err)
+    _,err = rows.Exec(admin, content)
+    checkErr(err)
+    log.Println("日志添加成功！")
 }
 
-//initthe database pointer
-func GetDBp(p *sql.DB){
-	db = p
-}
-
-//write the message into logfile, style control the way you record the log,
-//style use Err and Q_err will record the logs into error.log, 
-//style use Warn and Q_warn will record the logs into warning.log,
-//note that Q_warn and Q_err will not output message on consloe
-func Log(style int, msg ...interface{}){
-	path := getpath()
-	path = formatPath(path)
-	s := formatInterface(path, msg...)
-	if style > 0 {
-		fmt.Println(s)
-	}else{
-		style = 0-style
-	} 
-	switch style {
-	case Err :
-		errlog.Println(s)
-	case Warn:
-		warnlog.Println(s)
-	}
-}
-
-//compare to fmt.Println, it Println will display the caller and record intto logfile
-func Println(any ...interface{}) {
-	path := getpath()
-	path = formatPath(path)
-	s := formatInterface(path , any...)
-	fmt.Println(s)
-	infolog.Println(s)
-}
-
-func Fatal(msg ...interface{}){
-	path := getpath()
-	path = formatPath(path)
-	s := formatInterface(path, msg...)
-	errlog.Println("Fatal !!!")
-	errlog.Println(s)
-	fmt.Println(s)
-	os.Exit(1)
-}
-
-//=======================================================================================
-//===================== the following is subfunction ==================================== 
-
-//return the user file location, like "main.go"
-func getpath()string{
-	//Log()/Println() --> getpath() -->  Caller()
-	pc,file,_,ok := runtime.Caller(2)
-	if ok==false{
-		return "???"
-	}
-	//get caller code file name
-	short := file
-	for i := len(file) - 1; i > 0; i-- {
-		if file[i] == '/' {
-			short = file[i+1:]
-			break
+func Getlogs()[]opelog{
+    var Opelog []opelog
+    if nowsearch!="" {
+        rows, err := db.Query("select logid,module,admin,logtime,content from oresql where module=$2",nowsearch) 
+        checkErr(err)
+        for rows.Next(){
+		var opelog opelog
+		err = rows.Scan(&opelog.Logid,&opelog.Module,&opelog.Admin,&opelog.Logtime,&opelog.Content)
+		if err != nil {
+			fmt.Println("showscan error:",err)
 		}
+        Opelog=append(Opelog,opelog)
 	}
-	short2 := ""
-	//get caller function name
-	fun := runtime.FuncForPC(pc)
-	pcname := fun.Name()
-	for i:=len(pcname)-1;i>0;i--{
-		if pcname[i] == '.' {
-			short2 = pcname[i+1:]
-			break;
+    }else {
+        rows, err := db.Query("select logid,module,admin,logtime,content from t_opelog") 
+        checkErr(err)
+        for rows.Next(){
+		var opelog opelog
+		err = rows.Scan(&opelog.Logid,&opelog.Logtime,&opelog.Admin,&opelog.Module,&opelog.Content)
+		if err != nil {
+			fmt.Println("showscan error:",err)
 		}
+        Opelog=append(Opelog,opelog)
 	}
-	return short + " -> "+short2 +"()"
-}
-
-//set the format of pathstring
-func formatPath(path string) string{
-	return  "[" + path + "] : "
-}
-
-//the raw ouput is like :[a,b,c]
-//and after it function handle, it will be : a  b  c
-func formatInterface(prefix string, any ...interface{})string{
-	formatstr := strings.Repeat("%v  ", len(any))
-	prefix += fmt.Sprintf(formatstr, any...)
-	return prefix
-}
-
-
-//create a new floder to save the logs file, return the path of new floder
-//note that you can not use Println and Log function in createfloder()
-func createfloder()string{
-	//1-> check logs_root if exist, create new directory if not exist
-	rd, err := ioutil.ReadDir(logs_root)
-	if err != nil {
-		fmt.Println("logs: Can not read logs_root !", err)
-		fmt.Println("logs: Trying to create make directory ：", logs_root)
-		err = os.Mkdir(logs_root, os.ModeDir)
-		if err!=nil {	//read and create floder fall 
-			pwd , _ := os.Getwd()
-			fmt.Println("logs: Can not create logs_root!",err)
-			fmt.Println("提示：程序当前运行的路径为 :",pwd)
-			fmt.Println("提示：如果发现以上目录与代码目录不一致，请用go run main.go 来启动程序")
-			os.Exit(1)
-		}		
-		//can not read and already make an directory
-		fmt.Println("logs: Create logs_roots scuess! :",logs_root)
-		rd , err = ioutil.ReadDir(logs_root)
-		if err!=nil {
-			fmt.Println("logs: An Error happen, create logs_roots but can't read it ,",err)
-			os.Exit(1)
-		}
-	}
-	tempPath := logs_root //the path to save the logs files
-	ps := getPathSeperator()
-	if new_log == 0 {	//2->if it is testing, all logs_files save in a same floder
-		tempPath = logs_root + ps + "2019-05-12#1" +ps
-	}else{ 	//3-> in classic model, create new floder for each running
-		filenum := 1
-		datestr := time.Now().Format("2006-01-02")
-		for _, fi := range rd {
-			if fi.IsDir() && len(fi.Name())>10 {
-				fd := fi.Name()[0:10]
-				if fd==datestr{
-					filenum++
-				}
-			}
-		}
-		tempPath = logs_root + ps +datestr + `#` + strconv.Itoa(filenum) + ps
-	}
-	err = os.Mkdir(tempPath, os.ModeDir)
-	if err!=nil && os. IsExist(err)==false {	//if the reason of mkdir err is because the directory already exist
-		fmt.Println("logs : Can not make directory in logs_root!",err)
-		os.Exit(1)
-	}
-	return tempPath
-}
-
-//get system pathseperator character
-func getPathSeperator() string {
-	ps := `\`
-	if os.PathSeparator==47 {	//PathSeparator of system
-		ps = `/`
-	}
-	return ps
+    }
+    fmt.Println("正在读取数据库")
+    return Opelog
 }
---
+++ logServer/logs/logsql.go
@@ -0,0 +1,54 @@
+package logs
+
+import (
+	//"../models"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type opelog struct {
+    Logid           string    `json:"logid"`
+    Module      	string    `json:"module"`
+    Admin           string    `json:"admin"`
+    Logtime         string    `json:"logtime"`
+    Content   		string    `json:"content"`
+}
+
+type classify struct {
+    Search           string    `json:"search"`
+}
+
+var nowcontent string;
+var nowsearch string;
+
+func checkErr(err error) {   //报错
+    if err != nil {
+        log.Println("出错啦!")
+        panic(err)
+    }
+}
+
+func Search(w http.ResponseWriter, r *http.Request){          //获取查询
+    w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+    w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
+    w.Header().Set("content-type", "application/json")
+    defer r.Body.Close()
+    con, err := ioutil.ReadAll(r.Body) //获取post的数据
+    checkErr(err)
+    su := &classify{}         //把json转换回来
+    json.Unmarshal([]byte(con), &su)
+    fmt.Println("客户端输入：")
+    fmt.Println("\tsearch:", su.Search)
+    nowsearch=su.Search;
+}
+
+func Display(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type") //header的类型
+    w.Header().Set("content-type", "application/json")
+    var opelog []opelog
+    opelog = Getlogs()
+	data, _ := json.Marshal(opelog)
+    fmt.Fprintf(w,string(data))
+}
